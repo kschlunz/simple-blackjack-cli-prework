fec0b21e93e515ca3b7e22593f63a48c9de7ec43
Kate Schlunz <schlunzk@gmail.com>
2018-02-27 19:27:54 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -18,11 +18,11 @@ end
 
 def get_user_input
   words = gets.chomp
-  words 
+  words
 end
 
 def end_game
-  # code #end_game here
+  puts  "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
