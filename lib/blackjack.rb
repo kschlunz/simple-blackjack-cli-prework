67a2e40de33f0e4e26861dedb90d92ba9f943668
Kate Schlunz <schlunzk@gmail.com>
2018-02-27 20:59:20 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -60,12 +60,15 @@ end
 
 def runner
   welcome
-  initial_round
+  card_total = initial_round
 
-  until hit? == 21
+  until card_total > 21
 
-  end_game
-end
-  display_card_total
+    card_total = hit?(card_total)
+
+    display_card_total(card_total)
+
+  end
+    end_game
 
 end
