f68d47d748d3812953af93f95de25caf5e8e3ef8
Kate Schlunz <schlunzk@gmail.com>
2018-02-27 20:06:32 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -58,11 +58,12 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner(card_total)
+def runner
   welcome
   initial_round
-  hit?
+
   until card_total == 21
+    hit?
   end_game
 end
   display_card_total
