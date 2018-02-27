45bad34bce72d3030e98db5b39600ac995c3e179
Kate Schlunz <schlunzk@gmail.com>
2018-02-27 20:00:32 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -59,5 +59,11 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  initial_round
+  hit
+  display_card_total
+  until card_total == 21
+  end_game
+end
 end
